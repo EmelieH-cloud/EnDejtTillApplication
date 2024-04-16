1aeb1f148543f97ef40cc5dd283f03949e4ac678
emeliehensfelt <emelie046@gmail.com>
2024-04-16 12:23:13 +0200

    klasser

--- Models/Card.cs
+++ Models/Card.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EnDejtTillApplication.Models
+﻿namespace EnDejtTillApplication.Models
 {
+    // kommentar 
     internal class Card
     {
     }
