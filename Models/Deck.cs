3a676ff34d6f5de47da876fe0214528e002cedc9
emeliehensfelt <emelie046@gmail.com>
2024-04-16 17:38:12 +0200

    setups

--- Models/Deck.cs
+++ Models/Deck.cs
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace EnDejtTillApplication.Models
-=======
-﻿namespace EnDejtTillApplication.Models
->>>>>>> e4039222bc8e49dcd75a9bd003e09baaff9e8c60
 {
     internal class Deck
     {
     }
 }
+
--- Models/Game.cs
+++ Models/Game.cs
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-﻿namespace EnDejtTillApplication.Models
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace EnDejtTillApplication.Models
->>>>>>> e4039222bc8e49dcd75a9bd003e09baaff9e8c60
+
 {
     internal class Game
     {
