cfaaeb4e1fc47ce9166fd9636151cabad82f230e
emeliehensfelt <emelie046@gmail.com>
2024-04-19 09:58:27 +0200

    ny sida

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace EnDejtTillApplication
+﻿using EnDejtTillApplication.Pages;
+
+namespace EnDejtTillApplication
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new DemoContentPage();
         }
     }
 }
---
+++ Pages/DemoContentPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace EnDejtTillApplication.Pages;
+
+public partial class DemoContentPage : ContentPage
+{
+    public DemoContentPage()
+    {
+        InitializeComponent();
+    }
+}
