54ea52e55f23d714db50fc5a87a595b7a917fc81
emeliehensfelt <emelie046@gmail.com>
2024-04-19 13:34:02 +0200

    reset

--- Pages/FlyoutPages/FlyoutContentPage1.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace EnDejtTillApplication.Pages.FlyoutPages;
-
-public partial class FlyoutContentPage1 : FlyoutPage
-{
-    public FlyoutContentPage1()
-    {
-        InitializeComponent();
-    }
-}
--- Pages/contentpages/DemoContentPage3.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace EnDejtTillApplication.Pages;
-
-public partial class DemoContentPage3 : ContentPage
-{
-    public DemoContentPage3()
-    {
-        InitializeComponent();
-    }
-}
